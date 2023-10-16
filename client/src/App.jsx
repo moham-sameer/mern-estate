f3b4f8d4182625f4a233f4357a3034d0a3070945
Mohammad Sameer <ms4492222@gmail.com>
2023-10-16 15:30:18 +0530

    create and run server

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+const App = () => {
+  
+  return (
+    <div>
+    <Router>
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sign-in' element={<Signin/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+    </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className='flex justify-center items-center h-screen'>Home</h1>
+    </div>
+  )
+}
+
+export default Home
