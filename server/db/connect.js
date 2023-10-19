510ba06689337b89db2eac08ccb5e89e090b9c2a
Mohammad Sameer <ms4492222@gmail.com>
2023-10-19 20:09:09 +0530

    adding changes

---
+++ server/db/connect.js
@@ -0,0 +1,2 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://ms4492222:x3Yj7LetlFPcwaHT@cluster0.0a3kt6t.mongodb.net/MyDB?retryWrites=true&w=majority')
--- server/index.js
+++ server/index.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const app = express()
+const Product = require('./model/product')
+require('./db/connect')
+app.use(express.json())
+app.get('/',async(req,res)=>{
+    try {
+        
+        const data = await Product.find({})
+        res.send(data)
+        console.log(data)
+    } catch (error) {
+        res.status(500).json({error,msg:"There might be a problem with the internet or the cluster"})
+        console.log(error)
+    }
+})
 
-app.get('/',(req,res)=>{
-    res.send("Hello World! How are you ?")
+app.post('/',async(req,res)=>{
+    try {
+        
+        const data = new Product(req.body);
+        const result = await data.save()
+        res.send(result)
+    } catch (error) {
+       res.status(500).json({error,msg:"There might be a problem with the internet or cluster"})
+       console.log(error)
+    }
 })
 
 app.listen(4444,console.log('server is listening on port 4444...'))
