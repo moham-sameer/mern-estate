1ac3f87224a0e3ef7f3541ee2f4268cdb46ca6c0
Mohammad Sameer <ms4492222@gmail.com>
2023-10-22 16:47:00 +0530

    sign-in backend

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { signup } = require('../controller/auth.control')
+const { signup, signin } = require('../controller/auth.control')
  const router = express.Router()
 
  router.route('/signup').post(signup)
+ router.route('/signin').post(signin)
 
  module.exports = router;
    
