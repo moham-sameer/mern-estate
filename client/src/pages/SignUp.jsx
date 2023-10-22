cfde5a12e4f7e4a4ad846c77fc38f18aa9eac3e1
Mohammad Sameer <ms4492222@gmail.com>
2023-10-22 16:14:26 +0530

    complete signup function

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,22 +1,78 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
+import axios from 'axios'
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    try {
+      
+      e.preventDefault();
+      setLoading(true)
+      const data = await axios.post("http://localhost:3000/api/auth/signup",
+       formData
+      );
+      if(data.success === false){
+        setLoading(false)
+        setError(data.message)
+        return;
+      }
+      setLoading(false)
+      setError(null)
+      navigate('/sign-in')
+      console.log(data)
+    } catch (error) {
+      setLoading(false)
+      setError(error.message)
+    }
+  };
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
-      <form className='flex flex-col gap-4'>
-        <input type='text' className='border p-3 rounded-lg ' id='username' placeholder='username' />
-        <input type='email' className='border p-3 rounded-lg ' id='email' placeholder='email' />
-        <input type='password' className='border p-3 rounded-lg ' id='password' placeholder='password' />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>sign up</button>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">SignUp</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          onChange={handleChange}
+          className="border p-3 rounded-lg "
+          id="username"
+          placeholder="username"
+        />
+        <input
+          type="email"
+          onChange={handleChange}
+          className="border p-3 rounded-lg "
+          id="email"
+          placeholder="email"
+        />
+        <input
+          type="password"
+          onChange={handleChange}
+          className="border p-3 rounded-lg "
+          id="password"
+          placeholder="password"
+        />
+        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+           {loading? 'Loading...':"Sign Up"}
+        </button>
       </form>
-      <div className='flex gap-2 mt-5'>
+      <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
-        <Link to={"/sign-in"}><span className='text-blue-700'>Sign in</span></Link>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Sign in</span>
+        </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
--- server/index.js
+++ server/index.js
@@ -1,18 +1,26 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const userRouter = require('./routes/user.route')
 const authRouter = require('./routes/auth.route')
 require('./db/connect')
+// const corsOptions = {
+//     origin: 'http://localhost:5173', // Replace with the actual origin of your React app
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,  // If you are using cookies or sessions
+//     optionsSuccessStatus: 204,
+//   };
+app.use(cors())
 app.use(express.json())
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
-app.use((err,req,res, next) =>{
- const statusCode = err.statusCode || 500;
- const message = err.message || "Internal Server Error";
+app.use((error,req,res, next) =>{
+ const statusCode = error.statusCode || 500;
+ const message = error.message || "Internal Server Error";
  return res.status(statusCode).json({
     success: false,
     statusCode,
     message,
  })
 })
-app.listen(4444,console.log('server is listening on port 4444...'))
+app.listen(3000,console.log('server is listening on port 3000...'))
